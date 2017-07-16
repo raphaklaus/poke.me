43e1a024291fb1bc81028f5c7fd6097ff084102a
raphaklaus <raphaklaus@gmail.com>
2017-07-16 16:42:15 -0300

    feat: add pokemon's router

--- pokemon.router.js
+++ pokemon.router.js
@@ -1,17 +1,19 @@
 const Pokemon = require('./pokemon.model.js');
 
-module.exports = (app) => {
+module.exports = (app, response) => {
   app.get('/pokemons', (req, res, next) => {
-    Pokemon.find().then(result => {
-      // todo: render pokemon list
-    }).catch(error => next(error));
+    Pokemon.find().then(models => response.send(req, res, models, 'pokemons')).catch(error => next(error));
   });
 
-  app.get('/pokemons/new', (req, res, next) => {
-    // todo: render new pokemon form
+  app.get('/pokemons/new', (req, res) => {
+    response.send(req, res, null, 'new');
   });
 
-  app.post('/pokemons', (req, res, next) => {
+  app.post('/pokemons', (req, res) => {
+    console.log('sdasdasd', req.body.name);
+    new Pokemon(req.body).save().then(data => {
+      response.send(req, res, data, null, 'pokemons', 'Pokémon created!');
+    });
     // todo: persist pokemon into the database and render back to pokemon list,
     // with a message telling the pokemon was created
   });
