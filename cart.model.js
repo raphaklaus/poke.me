d14b886a381af25e8a04140fdf88d83f7a9bafc6
raphaklaus <raphaklaus@gmail.com>
2017-07-17 01:31:44 -0300

    feat: enhance cart entity

--- cart.model.js
+++ cart.model.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose'),
 const Cart = new Schema({
   pokemons: [{
     type: mongoose.Schema.Types.ObjectId,
+    ref: 'Pokemon',
     required: true
   }],
   total: {
--- cart.router.js
+++ cart.router.js
@@ -1,13 +1,14 @@
 const Cart = require('./cart.model.js');
 
-module.exports = (app) => {
+module.exports = (app, response) => {
   app.get('/carts', (req, res, next) => {
-    Cart.find().then(result => {
-      // todo: render pokemons bought
+    Cart.find().then(models => {
+      response.send(req, res, models, 'purchased');
     }).catch(error => next(error));
   });
 
-  app.post('/carts', (req, res, next) => {
+  app.post('/carts', (req, res) => {
+    new Cart(req.body);
     // todo: persist pokemon into the database and render back to pokemon list,
     // with a message telling the pokemon was created
   });
